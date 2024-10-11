d01775e2c9311ad12d71a11b32a25cd6396a9fec
Rohan-Koli <rohankoli0610@gmail.com>
2024-10-11 18:56:14 +0530

    Fix merge conflict in vite.config.js and update App.jsx styling

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   // base:"/Food-website/",
-=======
   base:"/Food-website/",
->>>>>>> 28df214bd96179561b4fca5184e2e5e86f743137
 })
